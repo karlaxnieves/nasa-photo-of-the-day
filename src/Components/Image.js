bd45958b01b14914c10b6fcdcfa94f38228d4c1e
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-03-18 13:06:48 -0700

    adding styles to the image

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,13 +1,31 @@
 import React from "react";
+import styled from 'styled-components'
+
+const StyledImg = styled.img`
+width: 50%;
+  border-radius: 20px;
+  border: 15px inset #A0A0A0;.
+  transform: skewX(35deg);
+  transform: skewY(10deg);
+  transform: skew(35deg, 10deg);
+  transition: transform 1s;
+  }
+
+  &:hover{
+    transform: skewX(-20px);
+  }
+`
 
 const Img = props => { //Img = the props function, which is an object destructuring
     const { image } = props
     return (
-        <img
+        <StyledImg
             alt='APOD'
-            className='idkyet'
+            className='galaxy'
             src={image}
         />
+
+
     )
 }
 
