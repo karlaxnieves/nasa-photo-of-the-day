c5ddf7acfb66be7ab8b07418cba8666db16347c2
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-04-14 16:32:06 -0700

    got my image to display

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
-import axios from 'axios';
-import Copyright from './components/Copyright';
-import Date from './components/Date';
-import Image from './components/Image';
-import Summary from './components/Summary';
+import axios from 'axios'
+import Image from './components/Image'
+import Copyright from './components/Copyright'
+import Date from './components/Date'
+import Summary from './components/Summary'
 import Title from './components/Title'
+import "./App.css";
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({})
 
   useEffect(() => {
-    axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-      .then((res) => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then((res) => { //once the API is received, responsd with the function of setData's.data object
         setData(res.data)
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
       })
-  }, []);
-
-
-
+  }, []) //
 
   return (
     <div className="App">
