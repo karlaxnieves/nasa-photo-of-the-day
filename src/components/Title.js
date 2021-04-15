0e0d285930d0b6a4e8acfbf0a39095400c81b66a
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-04-15 14:23:41 -0700

    added a hover on my title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,22 @@
 import React from "react";
+import styled from 'styled-components'
+
+const StyledH1 = styled.h1`
+  color: blue;
+  transition: transform 1s;
+  
+  &:hover{
+  transform: scale(1.5);
+  }
+  `
 
 
 const Title = props => {
     const { title } = props
     return (
-        <h1>
+        <StyledH1>
             {title}
-        </h1>
+        </StyledH1>
     )
 }
 
